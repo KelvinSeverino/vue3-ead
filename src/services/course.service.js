30430fec5066e1c53d41c0ef088bc7faaa679a66
Kelvin Severino <kseveri@outlook.com>
2023-05-02 20:17:19 -0300

    Criado service e vuex de Cursos

---
+++ src/services/course.service.js
@@ -0,0 +1,15 @@
+import BaseService from "./base.service"
+
+export default class CourseService extends BaseService {
+
+    static async getCourses (params) {
+        //Retornando promise, que é uma estrutura de sucesso ou falha da funcao executada
+        return new Promise((resolve, reject) => {
+            //Executa instancia do base.service
+            this.request({auth: true})
+                .get('/courses', params)
+                .then(response => resolve(response.data)) //sucesso
+                .catch(error => reject(error.response)) //falha
+        })
+    }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex'
 
 import users from './modules/users/'
+import courses from './modules/courses/'
 
 export default createStore({
     state: { //Dados que serao guardados
@@ -12,6 +13,7 @@ export default createStore({
     actions: { //Metodos que gerao acao no projeto
     },
     modules: { //Organiza projeto em modulos
-    users
+        users,
+        courses
     }
 })
