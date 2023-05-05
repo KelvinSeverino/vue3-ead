f0634b268f22af742ac37f4cca55450a6be219ab
Kelvin Severino <kseveri@outlook.com>
2023-05-04 22:11:05 -0300

    Criar Service e Modulos do Supports

---
+++ src/services/support.service.js
@@ -0,0 +1,17 @@
+import BaseService from "./base.service"
+
+export default class SupportService extends BaseService {
+
+    static async getSupportsByLesson (lessonId) {
+        //Retornando promise, que é uma estrutura de sucesso ou falha da funcao executada
+        return new Promise((resolve, reject) => {
+            //Executa instancia do base.service
+            this.request({auth: true})
+                .get('/supports', {
+                    params: {lesson: lessonId} //Parametros enviados para API
+                })
+                .then(response => resolve(response.data)) //sucesso
+                .catch(error => reject(error.response)) //falha
+        })
+    }
+}
