3ae1927edcbc4dcbb8578140dc1a1971853213b5
Kelvin Severino <kseveri@outlook.com>
2023-05-04 21:53:19 -0300

    Remoção de Div invalida na Modules

--- src/views/modules/components/Modules.vue
+++ src/views/modules/components/Modules.vue
@@ -1,35 +1,33 @@
-<template>
-    <div>        
-        <div class="left">
-            <div class="card">
-                <div class="title bg-laravel">
-                    <span class="text">Modulos</span>
-                    <span class="icon far fa-stream"></span>
-                </div>
+<template>      
+    <div class="left">
+        <div class="card">
+            <div class="title bg-laravel">
+                <span class="text">Modulos</span>
+                <span class="icon far fa-stream"></span>
+            </div>
 
-                <div 
-                    v-for="module in modules" 
-                    :key="module.id"
-                    @click.prevent="toggleModule(module.id)"
-                    :class="[
-                        'modules',
-                        module.id == showModule ? 'active' : ''
-                    ]">
-                    <div class="name">
-                        <span class="text">{{ module.nome }}</span>
-                        <span class="icon fas fa-sort-down"></span>
-                    </div>
-                    <ul class="classes" v-show="module.id == showModule">
-                        <li 
-                            v-for="lesson in module.lessons" 
-                            :key="lesson.id"
-                            :class="{'active' : lesson.id === lessonInPlayer.id}"
-                            @click.prevent="addLessonInPlayer(lesson)">
-                            <span v-if="lesson.views.length > 0" class="check active fas fa-check"></span>
-                            <span class="nameLesson">{{ lesson.nome }}</span>
-                        </li>
-                    </ul>
+            <div 
+                v-for="module in modules" 
+                :key="module.id"
+                @click.prevent="toggleModule(module.id)"
+                :class="[
+                    'modules',
+                    module.id == showModule ? 'active' : ''
+                ]">
+                <div class="name">
+                    <span class="text">{{ module.nome }}</span>
+                    <span class="icon fas fa-sort-down"></span>
                 </div>
+                <ul class="classes" v-show="module.id == showModule">
+                    <li 
+                        v-for="lesson in module.lessons" 
+                        :key="lesson.id"
+                        :class="{'active' : lesson.id === lessonInPlayer.id}"
+                        @click.prevent="addLessonInPlayer(lesson)">
+                        <span v-if="lesson.views.length > 0" class="check active fas fa-check"></span>
+                        <span class="nameLesson">{{ lesson.nome }}</span>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
