ec131b336ea48e741d4bee3d7be7b3c6674d08ca
Kelvin Severino <kseveri@outlook.com>
2023-05-11 21:49:37 -0300

    Adicao de Menu das Rotas

---
+++ src/layouts/components/MenuComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <transition name="fade">
+            <ul id="mainMenu">
+                <li>
+                    <router-link :to="{name: 'ead.home'}">Início</router-link>                    
+                </li>
+                <li>
+                    <router-link :to="{name: 'ead.mysupports'}">Minhas Dúvidas</router-link>
+                </li>
+                <li>                    
+                    <a href="">Sair</a>
+                </li>
+            </ul>
+        </transition>        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MenuComponent'
+}
+</script>
